c30ce71aa8d16be84c068bcb1ce9a46cd3de994a
DrabantBY <yauheni.basalai@gmail.com>
2025-12-01 17:17:04 +0300

    chore(assets): add local fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +18,51 @@ export default defineConfig({
         },
       },
     },
+  },
+
+  experimental: {
+    fonts: [
+      {
+        name: "Flecha",
+        cssVariable: "--font-flecha",
+        provider: "local",
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/Flecha.woff2",
+              "./src/assets/fonts/Flecha.woff",
+            ],
+          },
+        ],
+      },
+
+      {
+        name: "MaisonMono",
+        cssVariable: "--font-maison-mono",
+        provider: "local",
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/MaisonMono.woff2",
+              "./src/assets/fonts/MaisonMono.woff",
+            ],
+          },
+        ],
+      },
+
+      {
+        name: "MaisonLight",
+        cssVariable: "--font-maison-light",
+        provider: "local",
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/MaisonLight.woff2",
+              "./src/assets/fonts/MaisonLight.woff",
+            ],
+          },
+        ],
+      },
+    ],
   },
 });
