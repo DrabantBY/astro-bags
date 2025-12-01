f3c210a6cfeec34f16b76277438ae915357f1ff5
DrabantBY <yauheni.basalai@gmail.com>
2025-12-01 16:59:48 +0300

    chore(config): add vite props

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: "preserve",
+    assets: "assets",
+  },
+
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "assets/js/[name]-[hash].js",
+          entryFileNames: "assets/js/[name]-[hash].js",
+          assetFileNames: "assets/[ext]/[name]-[hash][extname]",
+        },
+      },
+    },
+  },
+});
