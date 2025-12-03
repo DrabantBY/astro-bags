1ebc0781d1775b9fdb53f3a65b55b25f5ae99501
DrabantBY <yauheni.basalai@gmail.com>
2025-12-03 17:15:40 +0300

    feat(pages): add next pages

---
+++ src/const/navmenu.ts
@@ -0,0 +1,8 @@
+export const NAV_MENU = [
+  { href: "/", label: "home" },
+  { href: "/collection", label: "collection" },
+  { href: "/women", label: "women" },
+  { href: "/men", label: "men" },
+  { href: "/heritage", label: "heritage" },
+  { href: "/sustainability", label: "sustainability" },
+];
