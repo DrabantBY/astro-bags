272ea3eac426fcef5b5e74580c4658b1abc13099
DrabantBY <yauheni.basalai@gmail.com>
2025-12-04 23:08:18 +0300

    feat(auth): add auth shell

---
+++ src/solidjs/types.d.ts
@@ -0,0 +1,49 @@
+import type { JSX, Setter } from "solid-js";
+
+export namespace FormTypes {
+  type FieldMode =
+    | "search"
+    | "text"
+    | "url"
+    | "none"
+    | "email"
+    | "tel"
+    | "numeric"
+    | "decimal";
+
+  type FieldError = {
+    error: boolean;
+    errorMessage: string | undefined;
+  };
+
+  type FieldProps = {
+    type?: string;
+    inputMode?: FieldMode;
+    name?: string;
+    label?: string;
+    value?: string;
+    placeholder?: string;
+    onChange?: () => void;
+    onInput?: () => void;
+    pattern?: string;
+    required?: boolean;
+  };
+
+  type FormName = "LOGIN" | "RESET" | "SIGNUP";
+
+  type FormProps = {
+    setFormName: Setter<FormName>;
+  };
+
+  type FormList = Record<FormName, (props: FormProps) => JSX.Element>;
+
+  type FieldEvent = Event & {
+    currentTarget: HTMLInputElement;
+    target: HTMLInputElement;
+  };
+
+  type FormEvent = SubmitEvent & {
+    currentTarget: HTMLFormElement;
+    target: Element;
+  };
+}
