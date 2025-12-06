ce9015443476fa1e2dcf9b945518a2903a819728
DrabantBY <yauheni.basalai@gmail.com>
2025-12-06 15:47:59 +0300

    refactor(layout): update navmenu styles

--- src/const/navmenu.ts
+++ src/const/navmenu.ts
@@ -3,6 +3,4 @@ export const NAV_MENU = [
   { href: "/collection", label: "collection" },
   { href: "/women", label: "women" },
   { href: "/men", label: "men" },
-  { href: "/heritage", label: "heritage" },
-  { href: "/sustainability", label: "sustainability" },
 ];
